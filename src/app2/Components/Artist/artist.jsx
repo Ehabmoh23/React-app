4f858892192e7e40abbfed5a973c12ce154582e2
Ehabmoh23 <ehabmohammed236@gmail.com>
2023-10-09 00:14:10 +0300

    day2 added componnets

---
+++ src/app2/Components/Artist/artist.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Artist = () => {
+    return (
+        <div className='text-white'>
+            hello from artist
+        </div>
+    );
+}
+
+export default Artist;
--- src/components/home/home.jsx
+++
@@ -1,8 +0,0 @@
-import { Component } from "react";
-
-class home extends Component{
-    render(){
-        return <h1>hello from home</h1>
-    }
-}
-export default home;
